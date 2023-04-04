7cdb061047fb7bfc1faa9c4aabe5271553d47768
Andrew Kim <fasttom@hanyang.ac.kr>
2023-04-04 13:15:17 +0900

    Py3 version implemented

--- py3_mytranspose/mytranspose.py
+++ py3_mytranspose/mytranspose.py
@@ -1,87 +1,55 @@
-import numpy as np
-import pandas as pd
-
-def dim(x) -> list:
-    if not hasattr(x, "__len__"):
-        return []
-    elif ((len(x) == 0) | (type(x) == str)):
-        return []
-    else:
+def dim(x)->list:
+    # x_should be type which can be converted to list
+    if is_sequence(x):
         return [len(x)]+dim(x[0])
-
-def check_rectangular(x) -> bool:
-    is_rectangular = False
-    # element
-    if len(dim(x)) == 0 & hasattr(x, "__len__"):
-        is_rectangular = True
-    # 1d list case
-    elif len(dim(x)) == 1:
-        is_rectangular = True
-    # 2+ dim case
-    elif len(dim(x)) >= 2:
-        is_rectangular = True
-        n_row = dim(x)[0]
-        n_col_first = dim(x)[1]
-        for r_idx in range(n_row):
-            row = x[r_idx]
-            n_col = len(row)
-            if n_col != n_col_first:
-                is_rectangular = False
-                break
-    return is_rectangular
-
-
-def mytranspose_0d(x):
-    # consider 0d list/array as scalar
-    # return itself
-    if check_rectangular(x):
-        return x
     else:
-        print("Cannot transpose input")
-        print("input should be null or 1+ dimension rectangular numeric, list, array, dataframe, tensor")
-        print("returning itself")
-        return None
-def mytranspose_1d(x):
-    # consider 1d list/array as vector without axis
-    # that is it is neither row_vector and column vector
-    # retrun itself
-    # len(dim(x)) = 1
-    if check_rectangular(x):
-        return x
-    else:
-        print("Cannot transpose input")
-        print("input should be null or 1+ dimension rectangular numeric, list, array, dataframe, tensor")
-        print("returning itself")
-        return None
+        return []
 
-def mytranspose_2d_or_higher(x):
-    # consider 1d list/array as matrix
-    if check_rectangular(x):
-        xt = []
-        n_row, n_col = dim(x)[0], dim(x)[1]
-        for c_idx in range(n_col):
-            col = []
-            for r_idx in range(n_row):
-                col.append(x[r_idx][c_idx])
-            xt.append(col)
+def is_sequence(obj)->bool:
+    t = type(obj)
+    return hasattr(t, "__len__") and hasattr(t, "__getitem__") and (t != str)
+
+def is_rectangular(x)->bool:
+    dim_x = dim(x)
+    if len(dim_x) <= 1:
+        # it is null, scalar, or vector
+        # which is rectangular
+        rectangular = True
     else:
-        print("Cannot transpose input")
-        print("input should be null or 1+ dimension rectangular numeric, list, array, dataframe, tensor")
-        print("returning itself")
-        return None
+        try:
+            n_row = dim_x[0]
+            n_col_expected = dim_x[1]
+            rectangular = True
+            for row_idx in range(n_row):
+                col = x[row_idx]
+                if len(col) != n_col_expected: # un homogeneous row occuered
+                    rectangular = False
+                    break
+        except TypeError:
+            rectangular = False
+    return rectangular
+
+def mytranspose_upto_1d(x):
+    return x
+
+def mytranspose_from_2d(x):
+    xt = []
+    n_row, n_col = dim(x)[0], dim(x)[1]
+    for col_idx in range(n_col):
+        col = []
+        for row_idx in range(n_row):
+            col.append(x[row_idx][col_idx])
+        xt.append(col)
     return xt
 
 def mytranspose(x):
-    can_transpose = check_rectangular(x)
-    if can_transpose:
-        dim_x = dim(x)
-        if len(dim_x) == 0:
-            xt = mytranspose_0d(x)
-        elif len(dim_x) == 1:
-            xt = mytranspose_1d(x)
+    if is_rectangular(x):
+        if len(dim(x)) <= 1:
+            xt = mytranspose_upto_1d(x)
         else:
-            xt = mytranspose_2d_or_higher(x)
+            xt = mytranspose_from_2d(x)
     else:
-        xt = mytranspose_0d(x)
+        print(x)
+        print("is not rectangualr for first two dimension")
+        xt = None
     return xt
-
